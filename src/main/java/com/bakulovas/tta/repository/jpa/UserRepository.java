e21e6819aa805afac6993c58d74f74244d50f7b4
Alexander Bakulov <asbakulov@gmail.com>
2021-11-20 23:24:35 +0600

    add user repository

---
+++ src/main/java/com/bakulovas/tta/repository/jpa/UserRepository.java
@@ -0,0 +1,15 @@
+package com.bakulovas.tta.repository.jpa;
+
+
+import com.bakulovas.tta.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+
+    User findByLoginName(String loginName);
+
+
+}
