25b4b5f75bbff13064bad070bb1f0e070a95f708
Alexander Bakulov <asbakulov@gmail.com>
2021-12-06 22:06:45 +0600

    logging

--- src/main/java/com/bakulovas/tta/TtaApplication.java
+++ src/main/java/com/bakulovas/tta/TtaApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableSwagger2
 public class TtaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TtaApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
 	}
 
 }
--- src/main/java/com/bakulovas/tta/config/ServerConfig.java
+++ src/main/java/com/bakulovas/tta/config/ServerConfig.java
@@ -3,9 +3,7 @@ package com.bakulovas.tta.config;
 
 
 import lombok.Getter;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
